e12d664389cbb0e611dc8e41e2b8a7a92ccfe2bd
Tess <270054867@yoobeestudent.ac.nz>
2022-05-05 10:27:30 +1200

    Debugged, page & links displaying

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import HomePage from './HomePage';
 import PlantSelection from './PlantSelectionPage';
 import Planner from './PlannerViewPage';
-import { Route, Link, Router } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import NavBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Route exact path = "/" component = {HomePage}/>
-      <Route exact path = "/plantselection" component = {PlantSelection}/>
-      <Route exact path = "/planner" component = {Planner}/>
+      <Routes><Route path = "/" element = {<HomePage/>}/></Routes>
+      <Routes><Route path = "/plantselection" element = {<PlantSelection/>}/></Routes>
+      <Routes><Route path = "/planner" element = {<Planner/>}/></Routes>
     </div>
   );
 }
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function NavBar(){
     return(
         <ul>
-        <li><Link to = "/">Home</Link></li>
-        <li><Link to = "/plantselection">Plant Selection</Link></li>
-        <li><Link to = "/planner">Planner</Link></li>
+            <li><Link to = "/">Home</Link></li>
+            <li><Link to = "/plantselection">Plant Selection</Link></li>
+            <li><Link to = "/planner">Planner</Link></li>
         </ul>
     )
 }
