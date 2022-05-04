340a58b9fdef0809d5c6a12e78677f33063b2371
Tess <270054867@yoobeestudent.ac.nz>
2022-05-04 23:08:16 +1200

    Routing Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import HomePage from './HomePage';
 import PlantSelection from './PlantSelectionPage';
 import Planner from './PlannerViewPage';
+import { Route, Link, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
-      <PlantSelection/>
-      <Planner/>
+      <Route exact to = "/" component = {HomePage}/>
+      <Route exact to = "/plantselection" component = {PlantSelection}/>
+      <Route exact to = "/planner" component = {Planner}/>
     </div>
   );
 }
